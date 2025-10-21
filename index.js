10e5d0d4bddb2876b024883dd78d2ab272ec836b
Giovana040 <souza.barbosa.giovana@escola.pr.gov.br>
2025-10-21 09:27:29 -0300

    update

---
+++ index.js
@@ -0,0 +1,60 @@
+const pokemon_number = document.getElementById("pknum")
+const pokemon_name = document.getElementById("pkname")
+const pokemon_img = document.getElementById("pkimg")
+
+const pokemon_form = document.getElementById("form")
+const pokemon_imput = document.getElementById("imput")
+
+const pokemon_prev = document.getElementById("bnt-prev")
+const pokemon_next = document.getElementById("bnt-next")
+
+let searchPokemon = 1;
+
+
+const fetchpokemon = async (pokemon) => {
+    const APIResponse = await fetch
+    (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if(APIResponse.status ===200){
+        const data = await APIResponse.json();
+        return data;
+    }
+}
+
+
+const renderPokemon = async (pokemon)=> {
+    const data = await fetchpokemon(pokemon)
+    if(data){
+        pokemon_name.innerHTML = data.name;
+        pokemon_number.innerHTML = data.id;
+        pokemon_img.src = data[sprites][versions][generation-v][black-white][animated][front_default];
+    } else{
+        pokemon_name.innerHTML = Não Encontrado;
+        pokemon_number.innerHTML = ``
+        pokemon_img.style.display= nome;
+    }
+}
+
+
+
+
+pokemon_form.addEventListener("submit"), (Event)=>{
+    Event.preventDefault();
+    console.log("Enviando Formulario",pokemon_imput.value);
+    renderPokemon(pokemon_imput.value.toLowerCase());
+    ImageCapture.value = "";
+})
+
+pokemon_prev.addEventListener("click", ()=>{
+    if(searchPokemon > 1){
+        searchPokemon -=1
+        renderPokemon(searchPokemon)
+}
+})
+
+pokemon_next.addEventListener("click", ()=>{
+    searchPokemon +=1
+    renderPokemon(searchPokemon)
+})
+
+renderPokemon(searchPokemon)
+
